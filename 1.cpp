5f8eb87cbb4bc43a52f63871475a2ea5e124966c
Brandon Rabbitt <87943188+brandonrabbitt2@users.noreply.github.com>
2021-09-08 18:44:14 -0400

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int milesPerGallon, tankSize;
+
+    cout << "First, Enter Your Automobiles Gas Tank Size in Gallons: ";
+
+    cout << "Now, Enter the Miles Per Gallon Your Automobile Gets: ";
+
+    cout << "You will be able to drive roughly " << tankSize * milesPerGallon << " miles before a refuel is needed." << endl;
+
+    system("pause>0"); 
+    return 0; 
+} 
 
