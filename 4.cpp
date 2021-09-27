06d5c3fcd2b733d1fb970212c3097b3cfdfacae8
Brandon Rabbitt <87943188+brandonrabbitt2@users.noreply.github.com>
2021-09-27 09:07:45 -0400

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 int main() 
 {
-    int userInts, evenSum = 0, oddSum = 0; //Declare variables
+    int userInts, evenSum = 0, oddSum = 0; 
    
-    cout << "Enter a Set of Integers, Using a 0 to Indicate You Are Finished Entering Numbers: "; //Prompt user to input a set of integers
-    cin >> userInts; //Set of integers are stored
+    cout << "Enter a Set of Integers, Using a 0 to Indicate You Are Finished Entering Numbers: "; 
+    cin >> userInts; 
 
-    while (userInts != 0) { // Checks to see if the two operands are equal 
-        if (userInts % 2 == 0) { //Checks if the integers are odd or even
-            evenSum += userInts; //Even numbers are added to evenSum
+    while (userInts != 0) { 
+        if (userInts % 2 == 0) { 
+            evenSum += userInts; 
         }
         else {
-            oddSum += userInts; //Odd numbers are added to oddSum
+            oddSum += userInts; 
         }
         cin >> userInts;
     }
 
-    cout << "The Sum of the Even Numbers = " << evenSum << endl; //Sum of odd and even numbers are displayed to user
+    cout << "The Sum of the Even Numbers = " << evenSum << endl; 
     cout << "The Sum of the Odd Numbers = " << oddSum << endl;
     
     system("pause>0");
